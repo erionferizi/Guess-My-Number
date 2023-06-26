d28c5077efd35acb5bd885c1296ab3663c299e3b
erionferizi <erion.ferizi@outlook.com>
2023-06-26 10:35:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -61,5 +61,3 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-//asdasdasd
-//asdhasbhdsbahbdhhasbdh
